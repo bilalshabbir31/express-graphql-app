c938da06f9d59f3e584cd5adef63ee8ba496b95a
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-11-30 16:23:35 +0400

    add root url

--- server.js
+++ server.js
@@ -23,7 +23,9 @@ async function startServer() {
 
   // Attach Apollo middleware to Express
   app.use("/graphql", expressMiddleware(server));
-
+  app.get("/", (req, res) => {
+    res.status(200).json({message: "Welcome to the GraphQL Project"})
+  })
   // Start the Express server
   app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}/graphql`);
