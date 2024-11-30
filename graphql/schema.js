50f4b628192adbeb33531909772c5968e5dd424b
Muhammad Bilal <bilaltheofficial@gmail.com>
2024-11-30 15:44:47 +0400

    add schema

---
+++ graphql/schema.js
@@ -0,0 +1,44 @@
+import gql from "graphql-tag";
+
+export const typeDefs = gql`
+  type User {
+    id: ID!
+    email: String!
+    name: String
+    posts: [Post!]!
+  }
+
+  type Post {
+    id: ID!
+    title: String!
+    content: String
+    published: Boolean!
+    createdAt: String!
+    updatedAt: String!
+    author: User!
+  }
+
+  type Query {
+    users: [User!]!
+    user(id: ID!): User
+    posts: [Post!]!
+    post(id: ID!): Post
+  }
+
+  type Mutation {
+    createUser(email: String!, name: String): User!
+    createPost(
+      title: String!
+      content: String
+      published: Boolean
+      authorId: ID!
+    ): Post!
+    updatePost(
+      id: ID!
+      title: String
+      content: String
+      published: Boolean
+    ): Post!
+    deletePost(id: ID!): Post!
+  }
+`;
